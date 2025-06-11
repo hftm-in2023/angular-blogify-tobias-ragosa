b823e95e8594e477f39737d7703b61f623f8808d
Tobias Ragosa <tobias.ragosa@hftm.ch>
2025-06-11 23:38:04 +0200

    fix: unexpected any in app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-blogify-tobias-ragosa';
+  selectedColor = 'red';
+  showDetails = false;
+  animals = ['Dog', 'Cat', 'Bird'];
+  currentAnimal = 'Dog';
+  name: string | undefined;
+
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
+  }
+
+  changeAnimal(animal: string) {
+    this.currentAnimal = animal;
+  }
 }
