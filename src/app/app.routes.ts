2f51fa4a4afaf4d9a92b39517fabf648cbd6c1c1
Tobias Ragosa <tobias.ragosa@hftm.ch>
2025-08-07 21:24:43 +0200

    feat: back to overview button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,13 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./features/blog/blog.module').then((m) => m.BlogModule),
+      import('./features/blog/blog.routes').then((m) => m.blogRoutes),
   },
   {
     path: 'detail/:id',
     loadChildren: () =>
-      import('./features/blog-detail/blog-detail.module').then(
-        (m) => m.BlogDetailModule,
+      import('./features/blog-detail/blog-detail.routes').then(
+        (m) => m.blogDetailRoutes,
       ),
   },
   { path: 'demo', component: DemoComponent },
--- src/app/features/blog-detail/blog-detail.component.ts
+++ src/app/features/blog-detail/blog-detail.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-blog-detail',
   templateUrl: './blog-detail.component.html',
   styleUrls: ['./blog-detail.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatButtonModule],
 })
 export class BlogDetailComponent {
   private route = inject(ActivatedRoute);
