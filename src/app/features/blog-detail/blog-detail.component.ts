b9b00e9348416c806b9cc37e996ebe425a1d90fd
Tobias Ragosa <tobias.ragosa@hftm.ch>
2025-08-07 20:38:58 +0200

    feat: display blogdetails

--- src/app/features/blog-detail/blog-detail.component.ts
+++ src/app/features/blog-detail/blog-detail.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog-detail',
   templateUrl: './blog-detail.component.html',
   styleUrls: ['./blog-detail.component.scss'],
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
 })
 export class BlogDetailComponent {
   private route = inject(ActivatedRoute);
